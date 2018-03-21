00b5fadc45e43c39962750f7ac9e6e099a2128db
Adrian Filipescu <adrian.filipescu@gmail.com>
2018-03-21 23:08:38 +0200

    Fix imports

--- VoiceEngine/src/main/java/com/ing/hackathon2018/VoiceEngineApp.java
+++ VoiceEngine/src/main/java/com/ing/hackathon2018/VoiceEngineApp.java
@@ -1,10 +1,8 @@
 package com.ing.hackathon2018;
 
-import it.sauronsoftware.jave.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.File;
 
 @SpringBootApplication
 public class VoiceEngineApp {

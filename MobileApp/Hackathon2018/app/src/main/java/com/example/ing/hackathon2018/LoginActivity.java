6021568a469fdd39be553ea3b46404c0b740a509
Edward Popescu <eddie2292@gmail.com>
2018-03-22 03:13:58 +0200

    add register call

--- MobileApp/Hackathon2018/app/src/main/java/com/example/ing/hackathon2018/LoginActivity.java
+++ MobileApp/Hackathon2018/app/src/main/java/com/example/ing/hackathon2018/LoginActivity.java
@@ -1,12 +1,16 @@
 package com.example.ing.hackathon2018;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class LoginActivity extends AppCompatActivity {
     private Button btn_login;
@@ -16,6 +20,37 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         init();
+        new Thread(new Runnable(){
+
+            @Override
+
+            public void run() {
+                register();
+            }
+
+        }).start();
+    }
+
+    public String register() {
+        try {
+            MediaType JSON
+                    = MediaType.parse("application/json; charset=utf-8");
+
+            OkHttpClient client = new OkHttpClient();
+
+            Request request = new Request.Builder()
+//                    .url("http://10.1.3.207:8088/api/register")
+                    .url("http://10.1.4.48:8088/api/register")
+                    .post(RequestBody.create(JSON, ""))
+                    .build();
+            Response response = client.newCall(request).execute();
+            String resultId = response.body().string();
+            System.out.println("got: " + resultId);
+            return resultId;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+       return "";
     }
 
     private void init(){
